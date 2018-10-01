5db281dd164df2c535833392c6f63e471c6fd867
sxbc <regentc101@gmail.com>
2018-10-01 15:02:18 -0500

    solve conflicts for routes and unnecesary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
-  root to: "home#welcome"
+  resources :posts do
+    member do
+      put 'publish', 'review', 'cancel'
+    end
+  end
 
-  get "/dashboard",          to: "home#special_dashboard"
-  put "/posts/:id/publish",  to: "posts#publish"
-  put "/posts/:id/review",   to: "posts#review"
-  put "/posts/:id/cancel",   to: "posts#cancel"
+  get "/dashboard", to: "home#special_dashboard"
+  root to: "home#welcome"
 end
